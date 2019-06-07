3efd417917203c98fd10d968dc8f9bdd934782cc
Fabien <fabien.dubail@gmail.com>
2019-06-07 18:12:59 +0200

    put the datetime picker in a form

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <DateTimePicker :value="value"/>
+    <b-container>
+      <b-row>
+        <b-card>
+          <b-card-header>Date time picker for bootstrap vue</b-card-header>
+          <img alt="Vue logo" src="./assets/logo.png">
+          <b-card-body>
+            <b-form>
+              <b-form-group
+                label="Enter a date/time"
+                description="Click on the calendar icon to open the date/time picker"
+              >
+                <DateTimePicker :value="value"/>
+              </b-form-group>
+            </b-form>
+          </b-card-body>
+        </b-card>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import DateTimePicker from './components/DateTimePicker.vue';
-import moment, { Moment } from 'moment';
+import { Component, Vue } from "vue-property-decorator";
+import DateTimePicker from "./components/DateTimePicker.vue";
+import moment, { Moment } from "moment";
 
 @Component({
   components: {
-    DateTimePicker,
-  },
+    DateTimePicker
+  }
 })
 export default class App extends Vue {
   private value: Moment = moment();
-
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
