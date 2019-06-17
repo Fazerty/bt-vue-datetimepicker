1a09f10f76ba6d1db814b02e7dd97e41c8443531
Fabien <fabien.dubail@gmail.com>
2019-06-17 17:01:46 +0200

    Add missing dateTimePicker.d.ts

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -1,30 +1,29 @@
 // Import vue component
+import Vue from 'vue';
 import DateTimePicker from './components/DateTimePicker.vue';
 // import BootstrapVue from 'bootstrap-vue';
 
 // Vue.use(BootstrapVue);
 
 // Declare install function executed by Vue.use()
-export function install(Vue) {
-	if (install.installed) return;
-	install.installed = true;
-	Vue.component('bt-vue-datetimepicker', DateTimePicker);
+export function install(Vue: Vue) {
+  if ((install as any).installed) { return; }
+  (install as any).installed = true;
+  (Vue as any).component('bt-vue-datetimepicker', DateTimePicker);
 }
 
 // Create module definition for Vue.use()
 const plugin = {
-	install,
+  install,
 };
 
 // Auto-install when vue is found (eg. in browser via <script> tag)
 let GlobalVue = null;
 if (typeof window !== 'undefined') {
-	GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-	GlobalVue = global.Vue;
-}
+  GlobalVue = (window as any).Vue;
+} // else if (typeof global !== 'undefined') { GlobalVue = global.Vue;}
 if (GlobalVue) {
-	GlobalVue.use(plugin);
+  GlobalVue.use(plugin);
 }
 
 // To allow use as module (npm/webpack/etc.) export component
